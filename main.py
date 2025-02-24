7eb6688c83063263d27350e8265336d61fec1bc6
Sajjaduddin07 <mdsajjaduddin939@gmail.com>
2025-02-24 01:37:48 -0500

    new code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -15,3 +16,6 @@ app.add_middleware(
 @app.get("/api/data")
 def get_data():
     return {"message": "Hello from FastAPI!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
